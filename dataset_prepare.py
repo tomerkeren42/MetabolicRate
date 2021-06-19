791bb45fff75526aa78e10b3094ec0078b3b5359
Tomer Keren <tomerkeren42@gmail.com>
2021-06-19 11:46:23 +0300

    Changed to_use_df to df

--- dataset_prepare.py
+++ dataset_prepare.py
@@ -5,21 +5,20 @@ class DataSet:
 	def __init__(self, path):
 		self.dataset_path = path
 		self.df = None
-		self.to_use_df = None
 
 	def CreateDataFrame(self):
 		self.df = pd.read_csv(self.dataset_path)
 
 	def CreateSubSetDataFrame(self, columns):
-		self.to_use_df = pd.DataFrame(data=self.df, columns=columns)
+		self.df = pd.DataFrame(data=self.df, columns=columns)
 
 	def DataSetSort(self, column, ascending=False):
-		self.to_use_df = self.to_use_df.sort_values(by=column, ascending=ascending)
+		self.df = self.df.sort_values(by=column, ascending=ascending)
 
 	def DropMissingRows(self):
-		self.to_use_df = self.to_use_df.dropna(how="any")
+		self.df = self.df.dropna(how="any")
 
 	def ReplaceMissingRows(self, replacer=0):
-		self.to_use_df = self.to_use_df.fillna(value=replacer)
-		self.to_use_df.replace({".": str(replacer), "#NUM!": str(replacer), '#VALUE!': str(replacer)}, inplace=True)
+		self.df = self.df.fillna(value=replacer)
+		self.df.replace({".": str(replacer), "#NUM!": str(replacer), '#VALUE!': str(replacer)}, inplace=True)
 
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from dataset_prepare import DataSet
 from data_analysis import DataAnalyst
 all_columns = ['N', 'age', 'gender', 'RMR', 'Hight (m)', ' Weight (kg)', 'BMI', 'BF(%)', 'FM(kg)', 'FFM(kg)', 'abdominal (cm)', 'neck', 'exercise', 'exercise- kind', 'diabetes', 'Thyroidism', 'P_ diets', 'BS', 'GLU', 'HbA1C(%)', 'TG', 'HDL', 'LDL', 't.CHO', 'DATE_2', 'RMR_2', ' Weight (kg)_2', 'FM(%)_2', 'FM', 'FFM', 'weight(Dexa)', 'BMI(DEXA)']
+subset_columns = ["age", "RMR", "Hight (m)", " Weight (kg)", "BMI", "BF(%)", "FM(kg)", "abdominal (cm)", "neck"]
 
 path_to_dataset = 'dataset/16_3_RMR.csv'
 
 if __name__ == '__main__':
 	DataSet = DataSet(path=path_to_dataset)
 	DataSet.CreateDataFrame()
-	DataSet.CreateSubSetDataFrame(columns=["age", "RMR", "Hight (m)", " Weight (kg)", "BMI", "BF(%)", "FM(kg)", "abdominal (cm)", "neck"])
+	DataSet.CreateSubSetDataFrame(columns=subset_columns)
 	DataSet.ReplaceMissingRows(replacer=-1)
 	# DataSet.DataSetSort(column="age", ascending=False)
 	DataAnalyst = DataAnalyst(DataSet.to_use_df)
 	DataAnalyst.dim_reduction()
-
