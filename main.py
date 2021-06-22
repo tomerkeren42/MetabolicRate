6bd16aa443070ce0fcfb4191cc25f31d4372e95c
Tomer Keren <tomerkeren42@gmail.com>
2021-06-22 10:36:13 +0300

    Added MLP model for RMR prediction

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from dataset_prepare import DataSet
 from data_analysis import DataAnalyst
-from linear_model import DataPredictor
+from linear_ml_models import DataPredictor
+from dl_model import train, preprocess
+columns = ['age', 'RMR', 'Hight(m)', ' Weight(kg)', 'BMI', 'BF(%)', 'FM(kg)', 'FFM(kg)', 'neck']
 
-all_columns = ['N', 'age', 'gender', 'RMR', 'Hight (m)', ' Weight (kg)', 'BMI', 'BF(%)', 'FM(kg)', 'FFM(kg)', 'abdominal (cm)', 'neck', 'exercise', 'exercise- kind', 'diabetes', 'Thyroidism', 'P_ diets', 'BS', 'GLU', 'HbA1C(%)', 'TG', 'HDL', 'LDL', 't.CHO', 'DATE_2', 'RMR_2', ' Weight (kg)_2', 'FM(%)_2', 'FM', 'FFM', 'weight(Dexa)', 'BMI(DEXA)']
-subset_columns = ["age", "RMR", "Hight (m)", " Weight (kg)", "BMI", "BF(%)", "FM(kg)"]
-
-path_to_dataset = 'dataset/16_3_RMR.csv'
+path_to_dataset = 'dataset/15_6_RMR_ID_cmmon_equations_noID.csv'
 
 
 def create_dataset():
 	prepared_dataset = DataSet(path=path_to_dataset)
 	prepared_dataset.CreateDataFrame()
-	prepared_dataset.CountNaNInColumns()
-	prepared_dataset.CreateSubSetDataFrame(columns=subset_columns)
+	# prepared_dataset.CountNaNInColumns()
+	prepared_dataset.CreateSubSetDataFrame(columns=columns)
 	prepared_dataset.DropMissingRows()
 	# prepared_dataset.PrintDataDescription()
 	# prepared_dataset.ShowAllPlots()
@@ -26,14 +25,19 @@ def dataset_analysis():
 	pass
 
 
-def dataset_prediction():
+def dataset_ml_prediction():
 	data_predictor = DataPredictor(DataSet.df)
-	data_predictor.preprocess()
 	# data_predictor.ComputeMSELinearApproaches()
 	data_predictor.CompareModels()
+
+
+def dataset_dl_prediction():
+	X_train, X_test, y_train, y_test = preprocess(DataSet.df)
+	train(X_train, X_test, y_train, y_test)
 
 
 if __name__ == '__main__':
 	DataSet = create_dataset()
 	# dataset_analysis()
-	dataset_prediction()
+	# dataset_ml_prediction()
+	dataset_dl_prediction()
